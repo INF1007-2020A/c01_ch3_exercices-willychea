51747712d986f620b68b4073c0a8b16176bf4d09
willychea <heng-willy.chea@polymtl.ca>
2020-09-14 20:23:42 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -27,7 +27,7 @@ def to_celsius(temperature: float) -> float:
 
 
 def to_farenheit(temperature: float) -> float:
-    return temperature *  
+    return temperature * 9/5 + 32 
 
 
 def main() -> None:
