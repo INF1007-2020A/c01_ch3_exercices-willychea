76bd8216dc6e1c085f50491623e698c5362b64da
willychea <heng-willy.chea@polymtl.ca>
2020-09-14 20:27:15 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -21,7 +21,6 @@ def to_degrees(angle_rads: float) -> tuple:
     seconds = int((((math.degrees(angle_rads) - deg) * 60) - minutes) * 60)
     return deg, minutes, seconds
 
-commit test
 
 def to_celsius(temperature: float) -> float:
     return (temperature - 32) * 5/9
