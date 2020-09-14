57d47540458f8eb3d2aad8a5f5b56d11e3d65f0f
willychea <heng-willy.chea@polymtl.ca>
2020-09-14 20:22:22 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -23,7 +23,7 @@ def to_degrees(angle_rads: float) -> tuple:
 
 
 def to_celsius(temperature: float) -> float:
-    return (temperature * 9/5 ) + 32
+    return (temperature  9/5 ) + 32
 
 
 def to_farenheit(temperature: float) -> float:
